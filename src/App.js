823a2c9ed3f41c0ad3683db54675f18ee288ccca
Christina <thtranph@ncsu.edu>
2023-07-20 15:39:09 +0700

    core commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './App.css';
 import HomePage from './Containers/HomePage/HomePage';
 import Resume from './Containers/Resume/Resume';
 import WritingPortfolio from './Containers/Writing Portfolio/WritingPortfolio';
 import Projects from './Containers/Projects/Projects';
 
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage/>},
+  {path: '/Projects', element: <Projects/>},
+  {path: '/Resume', element: <Resume/>},
+  {path: '/WritingPortfolio', element: <WritingPortfolio/>},
+]);
 function App() {
-  return (
-    <WritingPortfolio></WritingPortfolio>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Components/Menu Item/MenuItem.js
+++ src/Components/Menu Item/MenuItem.js
@@ -2,33 +2,44 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import "./MenuItem.css";
+import { useNavigate } from "react-router-dom";
 
 function MenuItem() {
+  let navigate = useNavigate();
+
   return (
     <Row>
       <Col>
         <div className="menu_item">
           <Col style={{ textAlign: "center" }}>
             <Nav.Item>
-              <Nav.Link>PROJECTS</Nav.Link>
+              <Nav.Link onClick={() => navigate('/Projects')}>
+                PROJECTS
+              </Nav.Link>
             </Nav.Item>
           </Col>
 
           <Col style={{ textAlign: "center" }}>
             <Nav.Item>
-              <Nav.Link>RESUME</Nav.Link>
+              <Nav.Link onClick={() => navigate('/Resume')}>
+                RESUME
+              </Nav.Link>
             </Nav.Item>
           </Col>
 
           <Col style={{ textAlign: "center" }}>
             <Nav.Item>
-              <Nav.Link>WRITING PORTFOLIO</Nav.Link>
+              <Nav.Link onClick={() => navigate('/WritingPortfolio')}>
+                WRITING PORTFOLIO
+              </Nav.Link>
             </Nav.Item>
           </Col>
 
           <Col style={{ textAlign: "center" }}>
             <Nav.Item>
-              <Nav.Link>ABOUT ME</Nav.Link>
+              <Nav.Link onClick={() => navigate('/')}>
+                ABOUT ME
+                </Nav.Link>
             </Nav.Item>
           </Col>
         </div>
